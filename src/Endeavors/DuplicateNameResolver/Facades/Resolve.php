22e7aad36efcedcda39f0c2268b99588cb25e323
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2017-09-01 09:44:47 -0700

    allow for alternate datasources to be resolved at runtime

--- src/Endeavors/DuplicateNameResolver/Facades/Resolve.php
+++ src/Endeavors/DuplicateNameResolver/Facades/Resolve.php
@@ -2,7 +2,8 @@
 
 namespace Endeavors\DuplicateNameResolver\Facades;
 
-use  Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Facade;
+use Endeavors\DuplicateNameResolver\Exceptions\NameDataSourceException;
 /**
  * @see \Endeavors\DuplicateNameResolver\NameResolver
  */
@@ -14,10 +15,31 @@ class Resolve extends Facade {
 	 * @param  string $name
 	 * @return string
 	 */
-	public static function name($name = null)
+	public static function name($name = null, $schema = null)
 	{
+        static::runTimeResolution($schema);
+
 		return static::$app['duplicate.name.resolver']->resolve($name);
 	}
+
+    public static function runTimeResolution($schema)
+    {
+        $namespace = static::$app['config']->get('duplicate-name-resolver::config.options.' . $schema . '.namespace');
+        $class = static::$app['config']->get('duplicate-name-resolver::config.options.' . $schema . '.class');
+
+        if( null !== $schema ) {
+            static::$app['duplicate.name.resolver'] = static::$app->share(function($app) use($class, $namespace)
+		    {
+                $instance = ltrim($namespace . $class);
+
+                if(class_exists($instance) ) {
+                    return new \Endeavors\DuplicateNameResolver\NameResolver(new $instance);
+                }
+
+                throw new NameDataSourceException("Your datasource class " . $instance . " could not be found.");
+            });
+        }
+    }
 
 	/**
 	 * Get the registered name of the component.
--- src/config/config.php
+++ src/config/config.php
@@ -7,10 +7,15 @@ return array(
      |--------------------------------------------------------------------------
      |
      | Configure the class that implements the INameDataSource contract
-     |
+     | The namespace and class configured without a schema is the default
+     | Used when using Resolve::name without a second argument
      */
     'options' => array(
         'class' => 'NamesDataSource',
-        'namespace' => 'App\\'
+        'namespace' => 'App\\',
+        'templates' => [
+            'class' => 'NamesDataSource',
+            'namespace' => 'App\\'
+        ]
     )
 );
